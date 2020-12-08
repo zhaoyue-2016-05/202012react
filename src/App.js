ce7b7e0581a7009b1d341ef2340657c9a6b88607
zhaoyue-2016-05 <zhaoyue240143285@qq.com>
2020-12-08 11:26:45 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,32 @@
+/*
+应用根组件
+*/ 
+
+import React, { Component } from 'react'
+import './App.css';
+
+class App extends Component {
+
+  render () {
+    return (
+      <div className="App">
+      <header className="App-header">
+        后台管理1
+      </header>
+    </div>
+    )
+  }
+}
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         后台管理
+//       </header>
+//     </div>
+//   );
+// }
+
+export default App;
